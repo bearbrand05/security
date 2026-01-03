b5adad2721d30ae85db46e66b08de708396c5c7c
Serdon <johnrhealserdon@gmail.com>
2026-01-03 10:45:45 +0800

    Delete safeaccess directory

--- safeaccess/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS safeaccess_db;
-USE safeaccess_db;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
